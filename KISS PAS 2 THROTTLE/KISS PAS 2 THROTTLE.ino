320e55a7dc2fae99fecaac79b29ae487495dfe65
madhare75 <madhare75@gmail.com>
2021-08-25 23:39:35 -0500

    Changed the function call for the pulse sample.  Changed pulse range from 20-250ms, 250-500ms.  Added safeguard to throttle control output.

--- KISS PAS 2 THROTTLE/KISS PAS 2 THROTTLE.ino
+++ KISS PAS 2 THROTTLE/KISS PAS 2 THROTTLE.ino
@@ -1,45 +1,51 @@
 /*
+ VERSION: 1.0.1
+ 
  Name:		KISS_PAS_2_THROTTLE.ino
  Created:	8/23/2021 10:34:43 AM
  Author:	r4bb17
+
 */
 const int PASPin = 2;           // PAS Input pin
-const int LEDPin = 13;          // Built-in test LED
 const int PWMOut = 11;          // PWM Output pin
 
-const int lowPWMValue = 53;     // Low PWM value, 53 (.830 V)
-const int highPWMValue = 197;   // High PWM value, 197 (3.63V)
-int currentPWMValue;            // State of digital Throttle (53 - 197)
+const int lowPWMValue = 53;     // low PWM value, 53 (.830 V)
+const int highPWMValue = 197;   // high PWM value, 197 (3.63V)
+int currentPWMValue;            // state of digital throttle (53 - 197)
 
-unsigned long duration;         // Length of pulse in microseconds
-unsigned long pulses = 0;       // Total Pulses
+unsigned long duration;         // length of pulse in microseconds
+unsigned long pulses = 0;       // total pulses
 
 
 
 
 void setup() {
     Serial.begin(9600);         // Initialize Serial Port
-    pinMode(PASPin, INPUT);     // Initialize the PAS pin as a input
-    pinMode(LEDPin, OUTPUT);    // Initialize the LED as an output
-    pinMode(PWMOut, OUTPUT);    // Initialize the PWM pin as an output
+    pinMode(PASPin, INPUT);     // Initialize PAS pin as input
+    pinMode(PWMOut, OUTPUT);    // Initialize PWM pin as output
 }
 
 
 void loop() {
-    //Generate Idle Throttle Voltage.
-    turnOff();
-    
+    //generate Idle Throttle Voltage.
+    idleThrottle();
+
     //take a sample from the the Pedal Assist Sensor
-    duration = pulseIn(PASPin, HIGH);
+    duration = pulseInLong(PASPin, HIGH);
     unsigned int sample = duration / 1000;
 
     //increase pulse counter if valid pulse detected
-    // convert to (ms), anything over 800ms is unrelible [~ .62Hz lowest pulse detect]
-    if (sample < 251 && sample != 0) {
-        int mappedValue = map(sample, 20, 250, 197, 53);
-
-        analogWrite(PWMOut, mappedValue);
-        currentPWMValue = mappedValue;
+    // convert to (ms), anything over 500ms is unrelible [~1 Hz lowest pulse detect]
+    if (sample < 501 && sample != 0) {
+        //map sample value to high and low PWM range
+        int mappedValue = map(sample, 250, 500, highPWMValue, lowPWMValue);
+        
+        //safeguard throttle control from out of range anomalies.
+        if (mappedValue <= highPWMValue && mappedValue >= lowPWMValue) {
+            analogWrite(PWMOut, mappedValue);
+            currentPWMValue = mappedValue;
+        }
+        
         pulses++;
     }
 
@@ -48,7 +54,7 @@ void loop() {
 }
 
 
-void turnOff() {
+void idleThrottle() {
     //set throttle to idle
     currentPWMValue = lowPWMValue;
     analogWrite(PWMOut, lowPWMValue);
@@ -58,7 +64,7 @@ void turnOff() {
 void printDebug() {
     //print out diagnostic information
     Serial.print("Pulse duration: ");
-    Serial.print(duration/1000);
+    Serial.print(duration / 1000);
     Serial.print(" ms\t");
     Serial.print("Total Pulses = ");
     Serial.print(pulses);
